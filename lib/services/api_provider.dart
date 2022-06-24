96a6b277ee842a2378b81972d9b845a9e1f4e354
Angelo Montil <angelocool177@gmail.com>
2022-06-24 18:20:41 +0800

    Add api provider

---
+++ lib/services/api_provider.dart
@@ -0,0 +1,22 @@
+import 'package:cocktail_app/data/drink.dart';
+import 'package:cocktail_app/data/drinks.dart';
+import 'package:dio/dio.dart';
+
+class ApiProvider {
+  final Dio _dio = Dio();
+  final String _baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1';
+
+  Future<Drink> fetchRandomDrink() async {
+    Drinks allDrinks;
+    try {
+      final response = await _dio.get('$_baseUrl/random.php');
+      if (response.statusCode == 200) {
+        allDrinks = Drinks.fromMap(response.data);
+        return allDrinks.drinks[0];
+      }
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+    }
+    throw '';
+  }
+}
