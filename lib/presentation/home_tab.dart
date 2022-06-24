4ca0bbe1ca3e4eccb517dc30f665506f074278c5
Angelo Montil <angelocool177@gmail.com>
2022-06-24 21:48:21 +0800

    Implement persistent tab state

--- lib/presentation/home_tab.dart
+++ lib/presentation/home_tab.dart
@@ -4,11 +4,21 @@ import 'package:cocktail_app/data/drink.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeTab extends StatelessWidget {
+class HomeTab extends StatefulWidget {
   const HomeTab({Key? key}) : super(key: key);
 
   @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab>
+    with AutomaticKeepAliveClientMixin<HomeTab> {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     return BlocProvider(
       create: (context) => RandomBloc()..add(GetRandomDrink()),
       child: SingleChildScrollView(
